b58f0e958a9ac402616cbc7bcfaee0391de436f9
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-09-15 18:40:16 +0200

    Initial Commit

---
+++ weather-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weather-app
+//
+//  Created by Eirik Andresen on 15/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Eirik Andresen on 15/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
